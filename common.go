a985c5d1d02dbc63b5609ff5cf89d21253430a56
Khánh Vũ Hồ <qwerty22121998@gmail.com>
2022-05-27 22:30:56 +0700

    common fn

--- common.go
+++ common.go
@@ -11,6 +11,15 @@ var (
 	ErrMustIn   = func(arr interface{}) error {
 		return fmt.Errorf("value must be one of %v", arr)
 	}
+	ErrNotEqual = func(i interface{}) error {
+		return fmt.Errorf("value must be equal to %v", i)
+	}
+	ErrEqual = func(i interface{}) error {
+		return fmt.Errorf("value must not be %v", i)
+	}
+	ErrNotKind = func(k reflect.Kind) error {
+		return fmt.Errorf("value kind must be %v", k.String())
+	}
 )
 
 // Require check data is empty use IsZero method, if not return ErrRequired
@@ -32,5 +41,35 @@ func In(arr interface{}) Rule {
 			}
 			return ErrMustIn(arr)
 		})
+	})
+}
+
+// Equal check data equal to interface, if not return ErrNotEqual
+func Equal(i interface{}) Rule {
+	return RuleFn(func(data interface{}) error {
+		if !reflect.DeepEqual(data, i) {
+			return ErrNotEqual(i)
+		}
+		return nil
+	})
+}
+
+// NotEqual check data equal to interface, if equal return ErrNotEqual
+func NotEqual(i interface{}) Rule {
+	return RuleFn(func(data interface{}) error {
+		if reflect.DeepEqual(data, i) {
+			return ErrEqual(i)
+		}
+		return nil
+	})
+}
+
+// IsKind check data kind is given kind,if not return ErrNotKind
+func IsKind(kind reflect.Kind) Rule {
+	return RuleFn(func(data interface{}) error {
+		if reflect.ValueOf(data).Kind() != kind {
+			return ErrNotKind(kind)
+		}
+		return nil
 	})
 }
